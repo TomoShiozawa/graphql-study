3983222ed1e02dcfaac05f01cc40ed9a493f9edc
TomoShiozawa <18281856+TomoShiozawa@users.noreply.github.com>
2025-01-29 18:27:46 +0900

    feat: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,47 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// スキーマ定義
 const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
+  type SpecialMove {
+    id: ID!
+    name: String!
+    description: String
   }
 
   type Query {
-    books: [Book]
+    allSpecialMoves: [SpecialMove!]!
   }
 `;
 
-const books = [
+const specialMoves = [
   {
-    title: "The Awakening",
-    author: "Kate Chopin",
+    id: "1",
+    name: "北斗百裂拳",
+    description:
+      "北斗神拳の奥義の１つ。経絡秘孔を突くことで体の内部から爆発させる。",
   },
   {
-    title: "City of Glass",
-    author: "Paul Auster",
+    id: "2",
+    name: "かめはめ波",
+    description: "亀仙人が編み出した技。エネルギーを両手に集中させ放出する。",
   },
 ];
 
+// リゾルバ
 const resolvers = {
   Query: {
-    books: () => books,
+    allSpecialMoves: () => specialMoves,
   },
 };
 
+// サーバーインスタンスの生成
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+// サーバーの起動
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
 console.log(`🚀  Server ready at: ${url}`);
